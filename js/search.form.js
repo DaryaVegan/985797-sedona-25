24c7bdbbde60161a8cc6a17c9c68b39f70676281
DaryaVegan <47258972+DaryaVegan@users.noreply.github.com>
2019-06-11 22:07:11 +0300

    доработка js

--- js/search.form.js
+++ js/search.form.js
@@ -1,6 +1,7 @@
-var searchBtn = document.querySelector(".btn-search-hotel");
+var searchBtn = document.querySelector(".button-search-form");
 
 var	popup = document.querySelector(".ordering");
+
 searchBtn.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	popup.classList.toggle("ordering-show");
